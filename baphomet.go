e91fa2a77283809e0bf1e0de2572b0db7347a14c
aiubatti <aiubatti@edgelab.ch>
2021-11-04 11:15:31 +0100

    feat: mediate through Abraxas

--- baphomet.go
+++ baphomet.go
@@ -1,8 +1,18 @@
 package baphomet
 
-import "github.com/antonioiubatti93/baphomet/logger"
+import (
+	"github.com/antonioiubatti93/abraxas"
+	abraxaslog "github.com/antonioiubatti93/abraxas/logger"
+	"github.com/antonioiubatti93/baphomet/logger"
+)
 
 // Summon summons Baphomet.
 func Summon(logger logger.Logger) {
 	logger.Print("Hello, I'm Baphomet!")
+}
+
+func Mediate(logger abraxaslog.Logger) {
+	abraxas.Invoke(logger)
+	logger.Print("Summoning Baphomet now")
+	Summon(logger)
 }
--- baphomet_test.go
+++ baphomet_test.go
@@ -16,3 +16,12 @@ func Test_Summon(t *testing.T) {
 
 	assert.Equal(t, "Hello, I'm Baphomet!", hook.LastEntry().Message)
 }
+
+func Test_Mediate(t *testing.T) {
+	logger := log.New()
+	hook := logt.NewLocal(logger)
+
+	Mediate(logger)
+
+	assert.Equal(t, "Hello, I'm Baphomet!", hook.LastEntry().Message)
+}
