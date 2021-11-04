239d9306acd301c115ba45e66e017d7c248fdc7d
aiubatti <aiubatti@edgelab.ch>
2021-11-04 12:00:19 +0100

    feat: leveled logger

--- logger/logger.go
+++ logger/logger.go
@@ -5,3 +5,10 @@ type Logger interface {
 	Print(args ...interface{})
 	Printf(format string, args ...interface{})
 }
+
+type LeveledLogger interface {
+	Logger
+
+	Info(args ...interface{})
+	Infof(format string, args ...interface{})
+}
